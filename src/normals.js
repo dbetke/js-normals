9396b6ee6f88f5e6ba1ed9315bbd27110b1eb92a
dbetke <danielle.betke@gmail.com>
2013-03-26 15:32:26 -0400

    rearranges code to follow best practices

--- src/normals.js
+++ src/normals.js
@@ -1,136 +1,153 @@
 function generateNormals(str) {
     'use strict';
     var obj = {},
-        fileArray = str.split('\n');
+        fileArray = str.split('\n'),
+        item,
+        category,
+        state,
+        header,
+        wholeSubheader,
+        subheader,
+        description,
+        dataArray;
 
     obj.meta = {};
-    obj.data = {}; 
+    obj.data = {};
 
-    function lineCategory(str){
-        //determine type of line, set the case
+    function lineCategory(str) {
         var re_meta = new RegExp(/(.+):(.+)/),  //contains a semicolon with some content on either side
             re_header = new RegExp(/(.*)Normals(.*)/i), //contains the word 'normals', ignore case
-            re_subheader = new RegExp(/(Monthly)|(Daily)|(Annual\/Seasonal)/i),
-            re_dayNumbers = new RegExp(/^\s*(01)(.*)(31)$/),
-            re_dashes = new RegExp(/^-*$/),
-            re_blank = new RegExp(/^\s*$/);
+            re_subheader = new RegExp(/^(\D+)[^\-](\s*)$/), //one or more non-digit character, no dashes, followed by spaces
+            re_dayNumbers = new RegExp(/^\s*(01)(.*)(31)$/), //begins with 01 and ends with 31
+            re_dashes = new RegExp(/^-*$/), //contains dashes from the beginning to the end 
+            re_blank = new RegExp(/^\s*$/),
+            lineType; //contains spaces from the beginning to the end
 
         if (str.match(re_meta)) {
-            return 'meta';
+            lineType = 'meta';
         } else if (str.match(re_header)) {
-            return 'header';
+            lineType = 'header';
         } else if (str.match(re_subheader)) {
-            return 'subheader';
+            lineType = 'subheader';
         } else if (str.match(re_dayNumbers)) {
-            return 'daynumbers';
-        } else if (str.match(re_dashes)){
-            return 'dashes';
-        } else if (!str.match(re_blank)){
-            return 'data';
+            lineType = 'daynumbers';
+        } else if (str.match(re_dashes)) {
+            lineType = 'dashes';
+        } else if (!str.match(re_blank)) {
+            lineType = 'data';
         } else {
-            return '';
+            lineType = '';
         }
+
+        return lineType;
 
     }
 
-    function handleMetaData (str) {
+    function handleMetaData(str) {
         var newItem = str.split(':'),
             name = newItem[0].replace(/^[ \t]+|[ \t]+$/, ''),
             value = newItem[1].replace(/^[ \t]+|[ \t]+$/, '');
         obj.meta[name] = value;
     }
 
-    for (var item in fileArray) {
-        var category = lineCategory(fileArray[item]),
-            state = 1,
-            header,
-            wholeSubheader,
-            subheader,
-            description;
+    for (item in fileArray) {
+        category = lineCategory(fileArray[item]);
+        state = 1;
+
         //.. parse through each array element (line), skipping blanks
         if (category !== '') {
-            switch(state) {
-                case 1 : 
-                    switch(category) {
-                        case 'meta' :
-                            handleMetaData(fileArray[item]);
-                            state = 2;
-                        break;
-                 }
-                
-                case 2 :
-                    switch(category) {
-                        case 'meta' :
-                            handleMetaData(fileArray[item]);
-                            state = 2;
-                        break;
-                        case 'header' :
-                            header = fileArray[item];
-                            obj.data[header] = {};
-                            state = 3;
-                        break;
+            switch (state) {
+            case 1:
+                switch (category) {
+                case 'meta':
+                    handleMetaData(fileArray[item]);
+                    state = 2;
+                    break;
+                }
+
+            case 2:
+                switch (category) {
+                case 'meta':
+                    handleMetaData(fileArray[item]);
+                    state = 2;
+                    break;
+                case 'header':
+                    header = fileArray[item];
+                    obj.data[header] = {};
+                    state = 3;
+                    break;
+                }
+
+            case 3:
+                switch (category) {
+                case 'subheader':
+                    wholeSubheader = fileArray[item].split(' '); //for cases when the additional column header values are on the subheader line
+                    subheader = wholeSubheader[0];
+                    obj.data[header][subheader] = {};
+                    break;
+                }
+
+            case 4:
+                switch (category) {
+                case 'daynumbers':
+                    state = 4;
+                    break;
+
+                case 'dashes':
+                    state = 5;
+                    break;
+                }
+
+            case 5:
+                switch (category) {
+                case 'data':
+
+                    dataArray = fileArray[item].trim().split(/\s+/);
+
+                    for (item = 0; item < dataArray.length; item++) {
+                        //remove non-existent normals denoted by entries like -7777C, -8888, etc
+                        if (dataArray[item].match(/^-\d{4}\D?$/)) {
+                            dataArray[item] = "";
+                        }
                     }
 
-                case 3 : 
-                    switch(category) {
-                        case 'subheader' :
-                            wholeSubheader = fileArray[item].split(' '); //for cases when the additional column header values are on the subheader line
-                            subheader = wholeSubheader[0];
-                            obj.data[header][subheader] = {};
-                        break;
+                    //normals description
+                    if (dataArray[0].match(/^.+-/)) {
+                        description = dataArray[0];
+                        obj.data[header][subheader][description] = [];
+                        //month
+                        if (dataArray[1].match(/\D\D\D/)) {
+                            dataArray.splice(0, 2);
+                        } else {
+                            dataArray.splice(0, 1);
+                        }
+                        //month  
+                    } else if (dataArray[0].match(/\D\D\D/)) {
+                        dataArray.splice(0, 1);
                     }
 
-                case 4 : 
-                    switch(category) {
-                        case 'daynumbers' : 
-                            state = 4;
-                        break;
-                        
-                        case 'dashes' : 
-                            state = 5;
-                        break;
+                    //for daily, will be pushing multiple arrays
+                    if (subheader === 'Daily') {
+                        obj.data[header][subheader][description].push(dataArray);
+                    } else {
+                        if (dataArray.length === 1) {
+                            obj.data[header][subheader][description] = dataArray[0];
+                        } else {
+                            obj.data[header][subheader][description] = dataArray;
+                        }
                     }
 
-                case 5 : 
-                    switch(category) {
-                        case 'data' :
-                            var dataArray = fileArray[item].trim().split(/\s+/);
-                        
-                            if (dataArray[0].match(/^.+-/)) {
-                                description = dataArray[0];
-                                obj.data[header][subheader][description] = [];
+                    state = 5;
 
-                                if (dataArray[1].match(/\D\D\D/)) {
-                                    dataArray.splice(0,2);
-                                } else {
-                                    dataArray.splice(0,1);
-                                }
-                                
-                            } else if (dataArray[0].match(/\D\D\D/)) {
-                                dataArray.splice(0,1);
-                            }
-
-                            if (subheader === 'Daily') {
-                                obj.data[header][subheader][description].push(dataArray);
-                            } else {
-                                if(dataArray.length === 1) {
-                                    obj.data[header][subheader][description] = dataArray[0];
-                                } else {
-                                    obj.data[header][subheader][description] = dataArray;
-                                }
-                            }
- 
-                            state = 5;
-                        break;
-                    }
+                    break;
+                }
 
                 break;
             }
         }
 
     }
-    
-    console.log(obj);
+
     return obj;
 
-};
+}
